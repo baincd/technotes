13b2ba41eb6d113f0bea8e8f8fb170d721fc30c7
Chris Bain <baincd@users.noreply.github.com>
2018-01-21 09:55:10 -0500

    Cleanup helpful-git-commands

--- git/helpful-git-commands.bash
+++ git/helpful-git-commands.bash
@@ -1,9 +1,13 @@
 # Return checked out branch-name/tag-name/commit-id
 git branch | sed -n 's/^\* //p' | sed 's/^(HEAD detached at \(.*\))/\1/'
+# Returns:
+#     branch-name
+#     tag-name
+#     a1b2c3d
 
 # Return checked out branch-name/tag-name reference
-# ex:
+git branch | grep "^* " | sed 's|^* (HEAD detached at \(.*\))|refs/tags/\1|' | sed 's|^* \(.*\)|refs/heads/\1|'
+# Returns:
 #      refs/heads/branch-name
 #      refs/tags/tag-name
-# *Note*: A detached HEAD pointing to a commit ID will incorrectly resolve to a refs/tags/tag-name
-git branch | grep "^* " | sed 's|^* (HEAD detached at \(.*\))|refs/tags/\1|' | sed 's|^* \(.*\)|refs/heads/\1|'
+# *Note*: A detached HEAD pointing to a commit ID will incorrectly resolve to a refs/tags/a1b2c3d
