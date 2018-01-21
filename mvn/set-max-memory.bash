3230ee497248cc6d2746d0f50aea3cdd94c05e08
Chris Bain <baincd@users.noreply.github.com>
2018-01-21 09:59:34 -0500

    Convert format of files to .xml or .bash

--- mvn/set-max-memory.bash
+++ mvn/set-max-memory.bash
@@ -1,6 +1,4 @@
 # Set Max Memory for Maven
 
 ### Set the enviroment variable
-```
 MAVEN_OPTS=-Xmx1024m
-```
--- mvn/update-version.bash
+++ mvn/update-version.bash
@@ -1,4 +1,2 @@
-```
 mvn versions:set -DgenerateBackupPoms=false
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=2.0
-```
