4a2b36193e42e2094f627b91ac25d92c7e1bbc3a
Chris Bain <baincd@users.noreply.github.com>
2018-01-21 09:13:57 -0500

    Rename alias.md to bash-aliases.bash

--- bash/bash-aliases.bash
+++ bash/bash-aliases.bash
@@ -1,8 +1,7 @@
-## ~/.bashrc
+# Add to ~/.bashrc
 
-```bash
 alias grepsrc='grep --exclude-dir=".svn" --exclude-dir=".git" --exclude-dir="target" '
 alias findsrc='find -not -path "*/.svn/*" -not -name .svn -not -path "*/.git/*" -not -name .git '
 alias ll='ls -l'
 alias ls='ls -F --color=auto --show-control-chars'
-```
+# Git aliases can be found in git/.gitconfig
