42100d33058f06f1d178816bcb41cdbae4744d53
Chris Bain <baincd@users.noreply.github.com>
2018-01-27 09:15:05 -0500

    Add helpful ls and less bash aliases

--- bash/bash-aliases.bash
+++ bash/bash-aliases.bash
@@ -2,6 +2,7 @@
 
 alias grepsrc='grep --exclude-dir=".svn" --exclude-dir=".git" --exclude-dir="target" '
 alias findsrc='find -not -path "*/.svn/*" -not -name .svn -not -path "*/.git/*" -not -name .git '
-alias ll='ls -l'
-alias ls='ls -F --color=auto --show-control-chars'
+alias lss='/usr/bin/ls'
+alias ls='ls -l -F --color=auto --show-control-chars'
+alias less='less -S'
 # Git aliases can be found in git/.gitconfig
