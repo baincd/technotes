df6f93356afae73609ab552b052610b61c74bc96
Chris Bain <baincd@users.noreply.github.com>
2018-01-27 12:57:29 -0500

    Clean up aliases

--- bash/bash-aliases.bash
+++ bash/bash-aliases.bash
@@ -1,8 +1,21 @@
 # Add to ~/.bashrc
 
-alias grepsrc='grep --exclude-dir=".svn" --exclude-dir=".git" --exclude-dir="target" '
-alias findsrc='find -not -path "*/.svn/*" -not -name .svn -not -path "*/.git/*" -not -name .git '
-alias lss='/usr/bin/ls'
+alias grep='grep --color=auto '
+alias grepsrc='grep --exclude-dir=".svn" --exclude-dir=".git" --exclude-dir="target" --exclude-dir="node_modules" '
+alias findsrc='find -not -path "*/.svn/*" -not -name .svn -not -path "*/.git/*" -not -name .git -not -path "*/node_modules/*" -not -name node_modules '
 alias ls='ls -l -F --color=auto --show-control-chars'
 alias less='less -S'
+
+# Colorized versions
+alias grepc='grep --color=always'
+alias grepsrcc='grepsrc --color=always'
+alias lsc='ls --color=always'
+alias lessc='less -R'
+
+# No options versions
+alias grepX='/bin/grep'
+alias findX='/usr/bin/find'
+alias lsX='/usr/bin/ls'
+alias lessX='/usr/bin/less'
+
 # Git aliases can be found in git/.gitconfig
