20cb5ac91114ba8a25e9811568a8bafc08733629
Chris Bain <baincd@users.noreply.github.com>
2018-06-02 12:41:15 -0400

    Remove Awesome Git Prompt

--- bash/setup-bash-prompt.bash
+++ bash/setup-bash-prompt.bash
@@ -8,12 +8,8 @@
 #     . ~/.bash-prompt.bash
 # fi
 #
-# # add default prompt (gps, gpb, or gpq)
-# # use gpq (or leave off) if awesome git prompt (#3) is unstalled
+# # add default prompt (gps, gpss, gpb, or gpq)
 # gpb
-#
-# 3) (optional) install awesome git prompt
-# git clone https://github.com/magicmonty/bash-git-prompt ~/.bash-git-prompt
 
 DEFAULT_PS1=$PS1
 
@@ -30,7 +26,6 @@ _PROMPT_COMMAND_NOTHING_='echo ""'
 
 alias gpq='PS1=$_PS1_NO_BRANCH_       PROMPT_COMMAND=$_PROMPT_COMMAND_NOTHING_'    # Git Prompt Quiet (no git info)
 alias gpb='PS1=$_PS1_WITH_GIT_BRANCH_ PROMPT_COMMAND=$_PROMPT_COMMAND_NOTHING_'    # Git Prompt Branch (include branch name - similar to default git prompt)
-alias gpba='gpq && if [ -f ~/.bash-git-prompt/gitprompt.sh ]; then source ~/.bash-git-prompt/gitprompt.sh; else echo "bash-git-prompt not installed!" && gpb; fi' # Git Prompt Branch AwesomeBash Verbose (fall back to gpb otherwise)
 alias gps='PS1=$_PS1_NO_BRANCH_       PROMPT_COMMAND=$_PROMPT_COMMAND_GIT_STATUS_' # Git Prompt Status (include short git status)
 alias gpss='PS1=$_PS1_NO_BRANCH_      PROMPT_COMMAND=$_PROMPT_COMMAND_GIT_SHORT_STATUS_' # Git Prompt branch Status and file count
 
@@ -44,23 +39,6 @@ gps
 
 # Create custom Bash prompts: http://ezprompt.net/
 
-
-GIT_PROMPT_ONLY_IN_REPO=1
-GIT_PROMPT_START="\[\033]0;\w\007\]\n\[\033[01;33m\]\w\[\033[m\]\[\033[0m\]"
-GIT_PROMPT_PREFIX="("
-GIT_PROMPT_SUFFIX=")"
-GIT_PROMPT_END="\n$ "
-GIT_PROMPT_SYMBOLS_PREHASH="commit:"
-GIT_PROMPT_BRANCH="\[\033[01;36m\]" # Change from magenta to Bold Cyan
-GIT_PROMPT_COMMAND_FAIL='\[\033[1;31m\]✘-_LAST_COMMAND_STATE_' # Change from red to bold red
-# Make same as .gitconfig colors
-GIT_PROMPT_STAGED=' \[\033[0;32m\]●' # Change from red to green
-GIT_PROMPT_CONFLICTS=' \[\033[1;31m\]✖' # Change from red to bold red
-GIT_PROMPT_CHANGED=' \[\033[1;31m\]✚' # Change from blue to bold red
-GIT_PROMPT_UNTRACKED=' \[\033[0;33m\]…' # Change from cyan to yellow
-# My New Values
-GIT_PROMPT_SYMBOLS_PRETAG="tag:"
-GIT_PROMPT_DETACHED_HEAD="\[\033[1;35m\]" # Set to [Bold Magenta]
 
 __git_status_cmd_for_prompt() {
   local type="$1"
