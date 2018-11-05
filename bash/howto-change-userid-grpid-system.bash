a080b93a4edf5add806b8ab60f0b262d8d1ea0ea
Chris Bain <baincd@users.noreply.github.com>
2018-11-04 20:25:28 -0500

    Add notes on how to change the userid and groupid system-wide

---
+++ bash/howto-change-userid-grpid-system.bash
@@ -0,0 +1,5 @@
+usermod -u <NEWUID> <LOGIN>    
+groupmod -g <NEWGID> <GROUP>
+find / -user <OLDUID> -exec chown -h <NEWUID> {} \;
+find / -group <OLDGID> -exec chgrp -h <NEWGID> {} \;
+usermod -g <NEWGID> <LOGIN>
