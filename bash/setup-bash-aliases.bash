cc81dd9703f99fd2e883353574dcc46738b496f1
Chris Bain <baincd@users.noreply.github.com>
2019-01-13 18:40:27 -0500

    Add command to popd multiple times or clean up dir stack

--- bash/setup-bash-aliases.bash
+++ bash/setup-bash-aliases.bash
@@ -46,6 +46,42 @@ function sd() { # swap dir
   fi
   pushdX > /dev/null;
 }
+function cdp() {
+  local error=
+  while :
+  do
+    if [ "$error" != "" ]; then
+      echo "ERROR: $error"
+      error=
+    else
+      dirs
+    fi
+
+    echo "#, rm #, rm #-#, or q"
+    echo -n "> "
+    read cmd
+    if [[ $cmd == q* ]]; then # quit
+      return 0
+    elif [[ $cmd =~ ^rm\ [0-9]+$ ]]; then # remove single item from stack
+      local num=`echo "$cmd" | sed -E 's/rm ([0-9]+)/+\1/'`
+      popdX $num > /dev/null
+    elif [[ $cmd =~ ^rm\ [0-9]+-[0-9]+$ ]]; then # remove range from stack
+      local num=`echo "$cmd" | sed -E 's/rm ([0-9]+)-([0-9]+)/\1/'`
+      local end_num=`echo "$cmd" | sed -E 's/rm ([0-9]+)-([0-9]+)/\2/'`
+      for i in `seq $num $end_num`; do
+        popdX "+$num"> /dev/null
+      done
+    elif [[ $cmd =~ ^[0-9]+$ ]]; then # pop items off top of stack up to #
+      for i in `seq 2 $cmd`; do # Remove regardless of if dir exists
+        popdX +1 > /dev/null
+      done
+      popdX > /dev/null
+      return 0
+    else
+      error="I don't understand!"
+    fi
+  done
+}
 
 _LAST_CD_TS=0
 function cd() {
